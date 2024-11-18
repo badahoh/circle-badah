08d66d3e8a8dc9a6be3cf662d4b617bb1440c942
badah <ohb234@newschool.edu>
2024-11-18 14:26:39 -0500

    add add

--- funstuff.js
+++ funstuff.js
@@ -5,3 +5,6 @@ let button = document.getElementById("button");
 button.addEventListener('click',function(event){
     console.log("button clicked!")
 });
+
+
+# i forgot how to check console of the html on the actual page oops how do i do that agin?
